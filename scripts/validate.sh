de59780a856a44f1bd101337d906f146eed251e3
Kevin Joiner <kevinjoiner@users.noreply.github.com>
2022-10-27 22:13:21 -0400

    Add go generate to CI

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 set -e
+go generate ./..
 golangci-lint run
 go mod tidy
 go mod verify
 unclean=$(git status --porcelain --untracked-files=no)
 if [ -n "$unclean" ]; then
-  echo "Encountered dirty repo!";
-  echo "$unclean";
-  exit 1;
+  echo "Encountered dirty repo!"
+  echo "$unclean"
+  exit 1
 fi
