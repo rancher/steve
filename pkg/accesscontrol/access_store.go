234a949ea691b603bd715b16d9dee86356bb3bc0
Alejandro Ruiz <alejandro.ruiz@suse.com>
2024-07-18 13:10:09 +0200

    refactor(accesscontrol): add unit test for CacheKey

--- pkg/accesscontrol/access_store.go
+++ pkg/accesscontrol/access_store.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"crypto/sha256"
 	"encoding/hex"
+	"hash"
 	"sort"
 	"time"
 
 	v1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/rbac/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/util/cache"
 	"k8s.io/apiserver/pkg/authentication/user"
 )
@@ -19,9 +21,20 @@ type AccessSetLookup interface {
 	PurgeUserData(id string)
 }
 
+type policyRules interface {
+	get(string) *AccessSet
+	getRoleBindings(string) []*rbacv1.RoleBinding
+	getClusterRoleBindings(string) []*rbacv1.ClusterRoleBinding
+}
+
+type roleRevisions interface {
+	roleRevision(string, string) string
+}
+
 type AccessStore struct {
-	users  *policyRuleIndex
-	groups *policyRuleIndex
+	users  policyRules
+	groups policyRules
+	roles  roleRevisions
 	cache  *cache.LRUExpireCache
 }
 
@@ -31,10 +44,10 @@ type roleKey struct {
 }
 
 func NewAccessStore(ctx context.Context, cacheResults bool, rbac v1.Interface) *AccessStore {
-	revisions := newRoleRevision(ctx, rbac)
 	as := &AccessStore{
-		users:  newPolicyRuleIndex(true, revisions, rbac),
-		groups: newPolicyRuleIndex(false, revisions, rbac),
+		users:  newPolicyRuleIndex(true, rbac),
+		groups: newPolicyRuleIndex(false, rbac),
+		roles:  newRoleRevision(ctx, rbac),
 	}
 	if cacheResults {
 		as.cache = cache.NewLRUExpireCache(50)
@@ -73,16 +86,34 @@ func (l *AccessStore) PurgeUserData(id string) {
 func (l *AccessStore) CacheKey(user user.Info) string {
 	d := sha256.New()
 
-	l.users.addRolesToHash(d, user.GetName())
-
 	groupBase := user.GetGroups()
 	groups := make([]string, len(groupBase))
 	copy(groups, groupBase)
 	sort.Strings(groups)
 
+	l.addRolesToHash(d, user.GetName(), l.users)
 	for _, group := range groups {
-		l.groups.addRolesToHash(d, group)
+		l.addRolesToHash(d, group, l.groups)
 	}
 
 	return hex.EncodeToString(d.Sum(nil))
+}
+
+var null = []byte{'\x00'}
+
+func (l *AccessStore) addRolesToHash(digest hash.Hash, subjectName string, rules policyRules) {
+	for _, crb := range rules.getClusterRoleBindings(subjectName) {
+		digest.Write([]byte(crb.RoleRef.Name))
+		digest.Write([]byte(l.roles.roleRevision("", crb.RoleRef.Name)))
+		digest.Write(null)
+	}
+
+	for _, rb := range rules.getRoleBindings(subjectName) {
+		digest.Write([]byte(rb.RoleRef.Name))
+		if rb.Namespace != "" {
+			digest.Write([]byte(rb.Namespace))
+		}
+		digest.Write([]byte(l.roles.roleRevision(rb.Namespace, rb.RoleRef.Name)))
+		digest.Write(null)
+	}
 }
--- pkg/accesscontrol/policy_rule_index.go
+++ pkg/accesscontrol/policy_rule_index.go
@@ -2,7 +2,6 @@ package accesscontrol
 
 import (
 	"fmt"
-	"hash"
 	"sort"
 
 	v1 "github.com/rancher/wrangler/v3/pkg/generated/controllers/rbac/v1"
@@ -20,13 +19,12 @@ type policyRuleIndex struct {
 	rCache              v1.RoleCache
 	crbCache            v1.ClusterRoleBindingCache
 	rbCache             v1.RoleBindingCache
-	revisions           *roleRevisionIndex
 	kind                string
 	roleIndexKey        string
 	clusterRoleIndexKey string
 }
 
-func newPolicyRuleIndex(user bool, revisions *roleRevisionIndex, rbac v1.Interface) *policyRuleIndex {
+func newPolicyRuleIndex(user bool, rbac v1.Interface) *policyRuleIndex {
 	key := "Group"
 	if user {
 		key = "User"
@@ -39,7 +37,6 @@ func newPolicyRuleIndex(user bool, revisions *roleRevisionIndex, rbac v1.Interfa
 		rbCache:             rbac.RoleBinding().Cache(),
 		clusterRoleIndexKey: "crb" + key,
 		roleIndexKey:        "rb" + key,
-		revisions:           revisions,
 	}
 
 	pi.crbCache.AddIndexer(pi.clusterRoleIndexKey, pi.clusterRoleBindingBySubjectIndexer)
@@ -70,30 +67,6 @@ func (p *policyRuleIndex) roleBindingBySubject(rb *rbacv1.RoleBinding) (result [
 		}
 	}
 	return
-}
-
-var null = []byte{'\x00'}
-
-func (p *policyRuleIndex) addRolesToHash(digest hash.Hash, subjectName string) {
-	for _, crb := range p.getClusterRoleBindings(subjectName) {
-		digest.Write([]byte(crb.RoleRef.Name))
-		digest.Write([]byte(p.revisions.roleRevision("", crb.RoleRef.Name)))
-		digest.Write(null)
-	}
-
-	for _, rb := range p.getRoleBindings(subjectName) {
-		switch rb.RoleRef.Kind {
-		case "Role":
-			digest.Write([]byte(rb.RoleRef.Name))
-			digest.Write([]byte(rb.Namespace))
-			digest.Write([]byte(p.revisions.roleRevision(rb.Namespace, rb.RoleRef.Name)))
-			digest.Write(null)
-		case "ClusterRole":
-			digest.Write([]byte(rb.RoleRef.Name))
-			digest.Write([]byte(p.revisions.roleRevision("", rb.RoleRef.Name)))
-			digest.Write(null)
-		}
-	}
 }
 
 func (p *policyRuleIndex) get(subjectName string) *AccessSet {
