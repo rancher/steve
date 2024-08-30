df80d48b90ff3561d478126806fb76aa4dca802b
Eric Promislow <epromislow@suse.com>
2024-08-30 15:37:11 -0700

    Add more fields to index when sql-caching is on.

--- pkg/stores/sqlproxy/proxy_store.go
+++ pkg/stores/sqlproxy/proxy_store.go
@@ -130,7 +130,8 @@ var (
 			{"status", "provider"},
 		},
 		gvkKey("management.cattle.io", "v3", "Node"): {
-			{"status", "nodeName"}},
+			{"status", "nodeName"},
+		},
 		gvkKey("management.cattle.io", "v3", "NodePool"): {
 			{"spec", "clusterName"}},
 		gvkKey("management.cattle.io", "v3", "NodeTemplate"): {
