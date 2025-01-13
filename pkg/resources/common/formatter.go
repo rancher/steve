437994a3e7835589531dbbdd17e486d92f98e971
Chad Roberts <chad.roberts@suse.com>
2025-01-13 13:33:06 -0500

    Show patch link on the API resource when patch permission is present and add patch ResourceMethod to the schema.

--- pkg/resources/common/formatter.go
+++ pkg/resources/common/formatter.go
@@ -98,6 +98,7 @@ func formatter(summarycache *summarycache.SummaryCache, asl accesscontrol.Access
 		}
 		hasUpdate := accessSet.Grants("update", gvr.GroupResource(), resource.APIObject.Namespace(), resource.APIObject.Name())
 		hasDelete := accessSet.Grants("delete", gvr.GroupResource(), resource.APIObject.Namespace(), resource.APIObject.Name())
+		hasPatch := accessSet.Grants("patch", gvr.GroupResource(), resource.APIObject.Namespace(), resource.APIObject.Name())
 
 		selfLink := selfLink(gvr, meta)
 
@@ -117,6 +118,15 @@ func formatter(summarycache *summarycache.SummaryCache, asl accesscontrol.Access
 			}
 		} else {
 			delete(resource.Links, "remove")
+		}
+		if hasPatch {
+			if attributes.DisallowMethods(resource.Schema)[http.MethodPut] {
+				resource.Links["patch"] = "blocked"
+			} else {
+				resource.Links["patch"] = u
+			}
+		} else {
+			delete(resource.Links, "patch")
 		}
 
 		if unstr, ok := resource.APIObject.Object.(*unstructured.Unstructured); ok {
