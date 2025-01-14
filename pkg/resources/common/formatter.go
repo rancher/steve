ff4706b590d6598d98d45aac839c7df335efadc8
Chad Roberts <chad.roberts@suse.com>
2025-01-14 15:11:03 -0500

    Added tests for new functionality and corrected disallowed method for patch

--- pkg/resources/common/formatter.go
+++ pkg/resources/common/formatter.go
@@ -120,10 +120,8 @@ func formatter(summarycache *summarycache.SummaryCache, asl accesscontrol.Access
 			delete(resource.Links, "remove")
 		}
 		if hasPatch {
-			if attributes.DisallowMethods(resource.Schema)[http.MethodPut] {
+			if attributes.DisallowMethods(resource.Schema)[http.MethodPatch] {
 				resource.Links["patch"] = "blocked"
-			} else {
-				resource.Links["patch"] = u
 			}
 		} else {
 			delete(resource.Links, "patch")
