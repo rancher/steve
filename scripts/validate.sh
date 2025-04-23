b1c1d64f799fbb8b850d4c33bbce715744c3f119
Eric Promislow <epromislow@suse.com>
2025-04-23 15:11:18 -0700

    Fix the validate script.

--- scripts/validate.sh
+++ scripts/validate.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
-go generate ./..
+go generate ./...
 golangci-lint run
 go mod tidy
 go mod verify
 unclean=$(git status --porcelain --untracked-files=no)
 if [ -n "$unclean" ]; then
   echo "Encountered dirty repo!"
+  git diff
   echo "$unclean"
   exit 1
 fi
