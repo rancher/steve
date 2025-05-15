f1677a27488e7ea6d930040d4425cf9fc0a951c9
Eric Promislow <epromislow@suse.com>
2025-05-15 13:24:33 -0700

    Fix an error: there is no sub-error to return here.

--- pkg/sqlcache/informer/listoption_indexer.go
+++ pkg/sqlcache/informer/listoption_indexer.go
@@ -57,7 +57,7 @@ const (
 	   )`
 	createFieldsIndexFmt = `CREATE INDEX "%s_%s_index" ON "%s_fields"("%s")`
 
-	failedToGetFromSliceFmt = "[listoption indexer] failed to get subfield [%s] from slice items: %w"
+	failedToGetFromSliceFmt = "[listoption indexer] failed to get subfield [%s] from slice items"
 
 	createLabelsTableFmt = `CREATE TABLE IF NOT EXISTS "%s_labels" (
 		key TEXT NOT NULL REFERENCES "%s"(key) ON DELETE CASCADE,
@@ -904,11 +904,11 @@ func getField(a any, field string) (any, error) {
 				for _, v := range t {
 					itemVal, ok := v.(map[string]interface{})
 					if !ok {
-						return nil, fmt.Errorf(failedToGetFromSliceFmt, subField, err)
+						return nil, fmt.Errorf(failedToGetFromSliceFmt, subField)
 					}
 					itemStr, ok := itemVal[subField].(string)
 					if !ok {
-						return nil, fmt.Errorf(failedToGetFromSliceFmt, subField, err)
+						return nil, fmt.Errorf(failedToGetFromSliceFmt, subField)
 					}
 					_, ok = resultMap[itemStr]
 					if !ok {
