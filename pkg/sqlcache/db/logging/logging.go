7b897105873c89d0aafa31f4e2fa4ee358b09e24
Alejandro Ruiz <alejandro.ruiz@suse.com>
2025-09-22 15:09:23 +0200

    Add debug logging for queries

---
+++ pkg/sqlcache/db/logging/logging.go
@@ -0,0 +1,71 @@
+package logging
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+const slowQueryThreshold = 50 * time.Millisecond
+
+type QueryLogEntry struct {
+	Timestamp    time.Time `json:"ts"`
+	Query        string    `json:"query"`
+	Params       []any     `json:"params"`
+	DurationNano int64     `json:"duration_nano"`
+}
+
+type QueryLogger interface {
+	Log(startTime time.Time, query string, params []any)
+}
+
+type NoopQueryLogger struct{}
+
+func (NoopQueryLogger) Log(_ time.Time, _ string, _ []any) {}
+
+type logger chan QueryLogEntry
+
+func (l logger) Log(start time.Time, query string, params []any) {
+	elapsed := time.Since(start)
+	l <- QueryLogEntry{
+		Timestamp:    time.Now(),
+		DurationNano: elapsed.Nanoseconds(),
+		Query:        query,
+		Params:       params,
+	}
+
+	if elapsed > slowQueryThreshold {
+		logrus.Warnf("Query took %s (threshold %s): %q, params %q\n", elapsed, slowQueryThreshold, query, params)
+	}
+}
+
+func StartQueryLogger(ctx context.Context, p string) (QueryLogger, error) {
+	if p == "" {
+		return &NoopQueryLogger{}, nil
+	}
+
+	c := make(logger)
+	f, err := os.Create(p)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		defer f.Close()
+		enc := json.NewEncoder(f)
+		for entry := range c {
+			if err := enc.Encode(entry); err != nil {
+				fmt.Printf("error writing query log: %+v\n", err)
+				return
+			}
+		}
+	}()
+	go func() {
+		<-ctx.Done()
+		close(c)
+	}()
+	return c, nil
+}
