c6b1e314df845bc25fd460a64215ead01110f648
Alejandro Ruiz <alejandro.ruiz@suse.com>
2025-10-22 18:18:40 +0200

    Refactor WithEncoding option initialization

--- pkg/sqlcache/db/encoding.go
+++ pkg/sqlcache/db/encoding.go
@@ -30,23 +30,36 @@ func init() {
 	gob.Register([]any{})
 
 	// Allow using JSON encoding during development
-	if enc := os.Getenv("CATTLE_SQL_CACHE_ENCODING"); enc == "json" {
-		defaultEncoding = &jsonEncoding{}
+	switch os.Getenv("CATTLE_SQL_CACHE_ENCODING") {
+	case "gob":
+		defaultEncoding = encodingForType(GobEncoding)
+	case "json":
+		defaultEncoding = encodingForType(JSONEncoding)
+	case "gob+gz":
+		defaultEncoding = encodingForType(GzippedGobEncoding)
+	case "json+gz":
+		defaultEncoding = encodingForType(GzippedJSONEncoding)
 	}
 }
 
-func WithEncoding(encoding Encoding) ClientOption {
+func encodingForType(encType Encoding) encoding {
+	switch encType {
+	case GobEncoding:
+		return &gobEncoding{}
+	case JSONEncoding:
+		return jsonEncoding{}
+	case GzippedGobEncoding:
+		return gzipped(&gobEncoding{})
+	case GzippedJSONEncoding:
+		return gzipped(jsonEncoding{})
+	}
+	// unreachable
+	return defaultEncoding
+}
+
+func WithEncoding(encType Encoding) ClientOption {
 	return func(c *client) {
-		switch encoding {
-		case GobEncoding:
-			c.encoding = &gobEncoding{}
-		case JSONEncoding:
-			c.encoding = jsonEncoding{}
-		case GzippedGobEncoding:
-			c.encoding = gzipped(&gobEncoding{})
-		case GzippedJSONEncoding:
-			c.encoding = gzipped(jsonEncoding{})
-		}
+		c.encoding = encodingForType(encType)
 	}
 }
 
@@ -136,10 +149,7 @@ func (j jsonEncoding) Encode(w io.Writer, obj any) error {
 		enc.SetIndent("", strings.Repeat(" ", j.indentLevel))
 	}
 
-	if err := enc.Encode(obj); err != nil {
-		return err
-	}
-	return nil
+	return enc.Encode(obj)
 }
 
 func (j jsonEncoding) Decode(r io.Reader, into any) error {
