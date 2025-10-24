7586cb7cce364308fc87d909fa155f99295c4e9d
Josh Meranda <joshua.meranda@suse.com>
2025-10-24 13:37:49 -0400

    [main] Use normal client for resource creation (#779)
    * do not use table client for create requests
    
    * do not use table client or update and delete requests
    
    * add Client method to testFactory
    
    ---------
    
    Co-authored-by: joshmeranda <joshua.meranda@gmail.com>

--- pkg/stores/sqlproxy/proxy_store.go
+++ pkg/stores/sqlproxy/proxy_store.go
@@ -755,7 +755,7 @@ func (s *Store) Create(apiOp *types.APIRequest, schema *types.APISchema, params 
 	input["apiVersion"], input["kind"] = gvk.ToAPIVersionAndKind()
 
 	buffer := WarningBuffer{}
-	k8sClient, err := metricsStore.Wrap(s.clientGetter.TableClient(apiOp, schema, namespace, &buffer))
+	k8sClient, err := metricsStore.Wrap(s.clientGetter.Client(apiOp, schema, namespace, &buffer))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -779,7 +779,7 @@ func (s *Store) Update(apiOp *types.APIRequest, schema *types.APISchema, params 
 
 	ns := types.Namespace(input)
 	buffer := WarningBuffer{}
-	k8sClient, err := metricsStore.Wrap(s.clientGetter.TableClient(apiOp, schema, ns, &buffer))
+	k8sClient, err := metricsStore.Wrap(s.clientGetter.Client(apiOp, schema, ns, &buffer))
 	if err != nil {
 		return nil, nil, err
 	}
@@ -850,7 +850,7 @@ func (s *Store) Delete(apiOp *types.APIRequest, schema *types.APISchema, id stri
 	}
 
 	buffer := WarningBuffer{}
-	k8sClient, err := metricsStore.Wrap(s.clientGetter.TableClient(apiOp, schema, apiOp.Namespace, &buffer))
+	k8sClient, err := metricsStore.Wrap(s.clientGetter.Client(apiOp, schema, apiOp.Namespace, &buffer))
 	if err != nil {
 		return nil, nil, err
 	}
