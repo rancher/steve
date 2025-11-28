f577896b1ef330c24026ee9895ad3a92f512fb83
Tom Lebreux <tom.lebreux@suse.com>
2025-11-28 10:24:14 -0500

    Use go tools for code generation (#885)
    * Migrate mockgen to gotools
    
    * Migrate msgp to gotools

--- pkg/sqlcache/informer/informer_test.go
+++ pkg/sqlcache/informer/informer_test.go
@@ -19,8 +19,8 @@ import (
 	"k8s.io/client-go/tools/cache"
 )
 
-//go:generate mockgen --build_flags=--mod=mod -package informer -destination ./informer_mocks_test.go github.com/rancher/steve/pkg/sqlcache/informer ByOptionsLister
-//go:generate mockgen --build_flags=--mod=mod -package informer -destination ./dynamic_mocks_test.go k8s.io/client-go/dynamic ResourceInterface
+//go:generate go tool -modfile ../../../gotools/mockgen/go.mod mockgen --build_flags=--mod=mod -package informer -destination ./informer_mocks_test.go github.com/rancher/steve/pkg/sqlcache/informer ByOptionsLister
+//go:generate go tool -modfile ../../../gotools/mockgen/go.mod mockgen --build_flags=--mod=mod -package informer -destination ./dynamic_mocks_test.go k8s.io/client-go/dynamic ResourceInterface
 
 func TestNewInformer(t *testing.T) {
 	type testCase struct {
