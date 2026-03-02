55a1a53001569f2d1fcc6e6b0fdfc235970134cc
Felipe Gehrke <felipe.gehrke@suse.com>
2026-03-02 12:45:57 -0300

    [v2.13] #53604 - VAI: Pod restart field returns stale value (#1035)
    * Draft
    
    * added unit test to ParseRestarts
    
    * added integration tests
    
    * added tests and more fixes
    
    * removed unecessary comment
    
    ---------
    
    Co-authored-by: Tom Lebreux <tom.lebreux@suse.com>

--- pkg/resources/virtual/virtual_test.go
+++ pkg/resources/virtual/virtual_test.go
@@ -10,11 +10,76 @@ import (
 	"github.com/rancher/steve/pkg/resources/virtual/common"
 	"github.com/rancher/steve/pkg/summarycache"
 	"github.com/rancher/wrangler/v3/pkg/summary"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
+
+func TestParseRestarts(t *testing.T) {
+	// Mock Now for deterministic tests
+	fixedNow := time.Unix(1737462000, 0) // 2026-01-21 13:00:00 UTC
+	now = func() time.Time {
+		return fixedNow
+	}
+	defer func() { now = time.Now }()
+
+	tests := []struct {
+		name          string
+		input         string
+		wantCount     int
+		wantTimestamp int64 // nil means no timestamp expected
+		wantErr       bool
+	}{
+		{
+			name:          "no restarts",
+			input:         "0",
+			wantCount:     0,
+			wantTimestamp: 0,
+		},
+		{
+			name:          "single restart with time (3h37m ago)",
+			input:         "1 (3h37m ago)",
+			wantCount:     1,
+			wantTimestamp: fixedNow.Add(-3*time.Hour - 37*time.Minute).UnixMilli(),
+		},
+		{
+			name:          "multiple restarts with time (3h38m ago)",
+			input:         "4 (3h38m ago)",
+			wantCount:     4,
+			wantTimestamp: fixedNow.Add(-3*time.Hour - 38*time.Minute).UnixMilli(),
+		},
+		{
+			name:    "invalid format",
+			input:   "invalid",
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := ParseRestarts(tt.input)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Len(t, result, 2)
+
+			count, ok := result["count"].(int64)
+			require.True(t, ok)
+			assert.Equal(t, int64(tt.wantCount), count)
+
+			require.NotNil(t, result["timestamp"])
+			timestamp, ok := result["timestamp"].(int64)
+			require.True(t, ok)
+			assert.Equal(t, tt.wantTimestamp, timestamp)
+		})
+	}
+}
 
 func TestTransformChain(t *testing.T) {
 	now = func() time.Time { return time.Date(1992, 9, 2, 0, 0, 0, 0, time.UTC) }
