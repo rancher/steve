fa6d15340bdce24582fd6a837bacb1918dd2b711
Eric Promislow <epromislow@suse.com>
2026-05-05 17:00:23 -0700

    Provide a better response for '--version'. (#1173)

--- scripts/build-bin.sh
+++ scripts/build-bin.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-CGO_ENABLED=0 go build -ldflags "-extldflags -static -s" -o ./bin/steve
+
+shopt -s extglob
+
+FULLTAG=$(git describe --tags --long)
+VERSION=$(echo $FULLTAG | cut -d- -f1)
+NUM=$(echo $FULLTAG | cut -d- -f2)
+COMMIT=$(echo $FULLTAG | cut -d- -f3)
+if [[ -n "${NUM:-}" ]] ; then
+    VERSION=$VERSION-$NUM
+fi
+if [[ -z "${COMMIT:-}" ]] then
+   COMMIT=$(git rev-parse --short HEAD)
+fi
+
+CGO_ENABLED=0 go build -ldflags "-extldflags -static -s -X github.com/rancher/steve/pkg/version.Version=$VERSION  -X github.com/rancher/steve/pkg/version.GitCommit=$COMMIT" -o ./bin/steve
